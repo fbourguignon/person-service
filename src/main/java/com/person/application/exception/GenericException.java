1d19a42c8d75e901f9a79df7cc9877e9cbe65acf
Franklin Bourguignon <franklin_bourguignon@hotmail.com>
2021-05-08 21:47:54 -0300

    feat:(add custom exceptions)

---
+++ src/main/java/com/person/application/exception/GenericException.java
@@ -0,0 +1,7 @@
+package com.person.application.exception;
+
+public class GenericException extends RuntimeException{
+    public GenericException(String message) {
+        super(message);
+    }
+}
